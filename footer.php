54957cd1cda7490562838879443a21028af14473
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-01-23 16:11:06 +0000

    footer base html updated

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+		<footer id="footer">
+			
+		</footer>
+	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script defer async type="text/javascript" src="js/scripts.js?v=1.0"></script>
 </body>
