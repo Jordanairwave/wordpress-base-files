c88b029510616bf36320e4f801dc686b614a2b53
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-01-23 16:09:20 +0000

    Base html added to footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script defer async type="text/javascript" src="js/scripts.js?v=1.0"></script>
+</body>
+
+</html>
