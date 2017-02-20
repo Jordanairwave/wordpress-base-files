4dbc88cc57d6cbfa2ec8f6f1f51c93a152f6ba36
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-02-20 15:27:40 +0000

    archive page created

---
+++ archive.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Base Wordpress Theme Archive
+ *
+ * @package WordPress
+ * @subpackage Base Wordpress Theme
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<?php get_header(); ?>
+
+	<div class="grid-container">
+
+		<article>
+		<?php
+		if ( have_posts() ) : 
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/post/content', 'excerpt' );
+			endwhile;
+		else :
+			get_template_part( 'template-parts/post/content', 'none' );
+		endif;	
+		?>
+		</article>
+
+		<?php get_sidebar(); ?>
+
+	</div>
+
+<?php get_footer(); ?>
