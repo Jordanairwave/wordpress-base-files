9bbce98e72840e4d9b1e8ee743d4928b8d5be976
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-02-20 15:49:35 +0000

    sidebar file created

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,14 @@
-		<aside id="sidebar">
-			
-			
-		</aside>
+<?php
+/**
+ * Displays sidebar
+ *
+ * @package WordPress
+ * @subpackage Base Wordpress Theme
+ * @since 1.0
+ * @version 1.0
+ */
+?>
+
+<aside id="sidebar">
+	<?php dynamic_sidebar('sidebar'); ?>
+</aside>
