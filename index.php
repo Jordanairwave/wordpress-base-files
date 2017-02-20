586f1bc6f14a11b68dd913ed79fe4bdf97e85e91
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-02-20 15:42:25 +0000

    index file created

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
-<?php get_header(); ?>
-		<article id="main-content">
 <?php
-    if ( have_posts() ) : while ( have_posts() ) : the_post();
-        get_template_part( 'template-parts/post/content', 'excerpt' );
-    endwhile;
-    else :
-        get_template_part( 'template-parts/post/content', 'none' );
-    endif;	
+/**
+ * F3 starter index
+ *
+ * @package WordPress
+ * @subpackage Base Wordpress Theme
+ * @since 1.0
+ * @version 1.0
+ */
+
 ?>
-		</article>
-<?php get_sidebar(); ?>
+
+<?php get_header(); ?>
+
+	<div id="page-wrap">
+
+		<div class="grid-<?php echo $col; ?>">
+		<?php
+		if ( have_posts() ) : 
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/post/content', 'excerpt' );
+			endwhile;
+		else :
+			get_template_part( 'template-parts/post/content', 'none' );
+		endif;	
+		?>
+		</div>
+
+		<?php get_sidebar(); ?>
+
+	</div>
+
 <?php get_footer(); ?>
