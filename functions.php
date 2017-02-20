15da0b1b0c1a464b9ccb34acf2c5a2ecc47eda50
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-02-20 16:12:34 +0000

    HTML 5 Support added
    search-form, comment-form, comment-list, gallery, caption,

--- functions.php
+++ functions.php
@@ -18,6 +18,18 @@ function f3_replace_jquery() {
 	}
 }
 add_action('init', 'f3_replace_jquery');
+
+/*
+ * Switch default core markup for search form, comment form, and comments
+ * to output valid HTML5.
+ */
+add_theme_support( 'html5', array(
+	'search-form',
+	'comment-form',
+	'comment-list',
+	'gallery',
+	'caption',
+) );
  
  
  ?>
