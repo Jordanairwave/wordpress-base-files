4492df75797314ace9bfdbd05b06e44d78982aef
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-02-20 16:17:49 +0000

    theme js file added

--- functions.php
+++ functions.php
@@ -60,6 +60,16 @@ function blank_add_styles() {
 	wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main-min.css' );
 }
 add_action( 'wp_enqueue_scripts', 'blank_add_styles' );
+
+//* Add JS
+function blank_add_scripts() {
+	wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/min/main-min.js', array ( 'jquery' ) , 1.1, true );
+	
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'blank_add_scripts' );
  
  
  ?>
