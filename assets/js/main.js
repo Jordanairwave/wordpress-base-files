979f2e305c49327a9263d42fab625390b03c4b8a
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-02-20 16:28:20 +0000

    js files added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
 var site = {
 	init: function() {
 
+		/* Navigation */
+		jQuery('.responsive-menu').click(function(e) {
+			e.preventDefault();
+			jQuery('header nav').slideToggle();
+		}); 
+		/* END - JS nav slide toggle */
+	  
+		/*** Remove inline styles at media queries ***/
+		jQuery(window).resize(function() {
+			if (window.innerWidth > 640) {
+				jQuery("header nav").removeAttr("style");
+			}
+		}); 
+		/* END - JS to remove inline styles to nav */
+	  
+		jQuery(".responsive-menu").click(function() {
+			jQuery(".down").toggleClass('downstart');
+			jQuery(".fade").toggleClass("fadestart");
+			jQuery(".up").toggleClass("upstart");
+		});
+
     }
 };
 
-$(document).ready(function(){ site.init(); });
+( function( $ ) {
+	jQuery(document).ready(function(){ site.init(); });
+} )( jQuery );
