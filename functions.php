4ac38577b227843a5c8e95d7c8797910091990cd
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-02-20 16:18:59 +0000

    Remove height and width from images

--- functions.php
+++ functions.php
@@ -75,6 +75,19 @@ add_action( 'wp_enqueue_scripts', 'blank_add_scripts' );
 add_theme_support('post-thumbnails');
 //add_image_size( 'twentyseventeen-featured-image', 2000, 1200, true );
 //add_image_size( 'twentyseventeen-thumbnail-avatar', 100, 100, true );
+
+/**
+ * Removes width and height attributes from image tags
+ */
+function remove_image_size_attributes( $html ) {
+    return preg_replace( '/(width|height)="\d*"/', '', $html );
+}
+
+// Remove image size attributes from post thumbnails
+add_filter( 'post_thumbnail_html', 'remove_image_size_attributes' );
+
+// Remove image size attributes from images added to a WordPress post
+add_filter( 'image_send_to_editor', 'remove_image_size_attributes' );
  
  
  ?>
