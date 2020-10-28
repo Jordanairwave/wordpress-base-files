7e1d72a0fedcaf0ca190575340d41ebec462878d
Jordan Haines <jordan@factor3.co.uk>
2020-10-28 20:31:06 +0000

    Replace Jquery function removed

--- functions.php
+++ functions.php
@@ -8,17 +8,6 @@
  * @version 1.0
  */
 
-//* Making jQuery to load from Google Library
-function blank_replace_jquery() {
-	if (!is_admin()) {
-		// comment out the next two lines to load the local copy of jQuery
-		wp_deregister_script('jquery');
-		wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', false , null, true);
-		wp_enqueue_script('jquery');
-	}
-}
-add_action('init', 'blank_replace_jquery');
-
 /*
  * Switch default core markup for search form, comment form, and comments
  * to output valid HTML5.
